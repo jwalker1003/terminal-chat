c47911c1c14dcd44e79ace2b7841f8242d73c865
brad <bradjwalker04@gmail.com>
2026-03-28 23:44:58 -0400

    Missed fix in Program.cs expecting a return from a void method;

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -5,7 +5,7 @@
         public static async Task Main(string[] args)
         {
             var clientManager = new ClientManager();
-            _ = await clientManager.StartTcpListener();
+            await clientManager.StartTcpListener();
         }
     }
 }
