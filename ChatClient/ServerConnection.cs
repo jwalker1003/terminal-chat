cc2383c8c1180f3cc428ca2841332f3dafacb9f0
brad <bradjwalker04@gmail.com>
2026-03-31 23:59:05 -0400

    Moved README.md to top-level; Added try catch to ChatClient;

--- ChatClient/ServerConnection.cs
+++ ChatClient/ServerConnection.cs
@@ -8,40 +8,63 @@ namespace ChatClient
     {
         public async Task ConnectToServer()
         {
-            Console.WriteLine("Connecting to server...");
-            using TcpClient tcpClient = new();
-            tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 9090); // Make configurable
-            Console.WriteLine("Connecting to server!");
+            try
+            {
+                Console.WriteLine("Connecting to server...");
+                using TcpClient tcpClient = new();
+                tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 9090); // Make configurable
+                Console.WriteLine("Connecting to server!");
 
-            var stream = tcpClient.GetStream();
-            var readTask = ReadMessages(stream);
-            var writeTask = WriteMessages(stream);
+                var stream = tcpClient.GetStream();
+                var readTask = ReadMessages(stream);
+                var writeTask = WriteMessages(stream);
 
-            await Task.WhenAny(readTask, writeTask);
+                await Task.WhenAny(readTask, writeTask);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
         }
 
         public async Task ReadMessages(NetworkStream stream)
         {
-            await Task.Run(async () =>
+            try
             {
-                byte[] buffer = new byte[1024];
-                while (true)
+
+                await Task.Run(async () =>
                 {
-                    _ = await stream.ReadAsync(buffer);
-                    var msg = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine(msg);
-                }
-            });
+                    byte[] buffer = new byte[1024];
+                    while (true)
+                    {
+                        _ = await stream.ReadAsync(buffer);
+                        var msg = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine(msg);
+                    }
+                });
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
         public async Task WriteMessages(NetworkStream stream)
         {
-            while (true)
+            try
             {
-                var input = Console.ReadLine();
-                if (input == ":q") break;
-                byte[] buffer = Encoding.UTF8.GetBytes(input ?? "");
-                await stream.WriteAsync(buffer).ConfigureAwait(false);
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    if (input == ":q") break;
+                    byte[] buffer = Encoding.UTF8.GetBytes(input ?? "");
+                    await stream.WriteAsync(buffer).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
         }
     }
